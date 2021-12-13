0b91fbac37a9a9e72aa920f0d71ffaedb267ead4
jacobbeacom <jdbeacom97@comcast.net>
2021-12-13 16:59:09 -0600

    now returns the userprojects array post-parse if something was found in localstorage

--- src/applogic/LocalStorage/checklocalstorage.js
+++ src/applogic/LocalStorage/checklocalstorage.js
@@ -1,13 +1,17 @@
 import { newProject } from "../Projects/createnewproject";
 
+let userProjects = [];
+
 function checkStorage () {
     if (Storage.length == 0) {
-       return newProject('New Project');
-    } else {
-        key = localStorage.key(0);
-        return localStorage.getItem(key);
-    }
+       let defaultProject = newProject('New Project');
+       userProjects.push(defaultProject);
 
+    } else {
+         return JSON.parse(window.localStorage.getItem('userProjects'))
+    }
 }
 
 export default checkStorage;
+
+//checkstorage will be called elsewhere, so we do not need to define a place for the stored userProjects array to go in this module
