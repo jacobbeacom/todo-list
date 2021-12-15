a9f725f8e024c2e3aaf46ae7e0d800978b07ffc0
jacobbeacom <jdbeacom97@comcast.net>
2021-12-15 16:27:12 -0600

    added function to populate project list on sidebar

---
+++ src/DOM/projectlist.js
@@ -0,0 +1,16 @@
+import { userProjects } from "../applogic/LocalStorage/checklocalstorage";
+
+function populateProjectList() {
+    const projectList = document.getElementById('projectList');
+    const projects = userProjects;
+
+    projects.forEach(function(element) {
+        const listItem = document.createElement('button');
+        listItem.classList.add('projectListItem');
+        listItem.textContent = element.name;
+        listItem.setAttribute('id', `${element.name}`);
+        projectList.appendChild(listItem);
+    })
+}
+
+export default populateProjectList;
