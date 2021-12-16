f3f0d94ab4ecc925e35ab2d0fc825cb1fca75a93
jacobbeacom <jdbeacom97@comcast.net>
2021-12-16 12:06:51 -0600

    removed console.logs from projectlist module

--- src/DOM/projectlist.js
+++ src/DOM/projectlist.js
@@ -3,14 +3,12 @@ import { checkStorage } from "../applogic/LocalStorage/checklocalstorage";
 function populateProjectList() {
     const projectList = document.getElementById('projectList');
     let list = checkStorage();
-    console.log(list, 'projectlist 1');
 
     projectList.innerHTML = "";
 
     for (let i = 0; i < list.length; i++) {
         const listItem = document.createElement('button');
         listItem.classList.add('projectListItem');
-        console.log(list[0], i, 'projectlist 2');
         listItem.textContent = `${list[i].name}`;
         listItem.setAttribute('id', `${list[i].name}`);
         projectList.appendChild(listItem);
