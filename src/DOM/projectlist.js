db8043696c1c72dd75619d36e0435ed87a7d49dc
jacobbeacom <jdbeacom97@comcast.net>
2021-12-16 12:42:51 -0600

    selecting a project from the list now sets it as active

--- src/DOM/projectlist.js
+++ src/DOM/projectlist.js
@@ -11,6 +11,17 @@ function populateProjectList() {
         listItem.classList.add('projectListItem');
         listItem.textContent = `${list[i].name}`;
         listItem.setAttribute('id', `${list[i].name}`);
+        listItem.addEventListener('click', (e) => {
+            if (e.target.classList.contains('active')) { 
+                return;
+            }   else {
+                    const buttons = document.getElementsByClassName('projectListItem');
+                    for (let p = 0; p < buttons.length; p++) {
+                        buttons[p].classList.remove('active');
+                    }
+                    e.target.classList.add('active');
+            }
+        });
         projectList.appendChild(listItem);
     }
 }
