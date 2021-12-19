243a2bc8f83747caaf5860ee3900c3a188ae4e78
jacobbeacom <jdbeacom97@comcast.net>
2021-12-19 13:55:16 -0600

    corrected formatting errors from eslint

--- src/DOM/projectoverview.js
+++ src/DOM/projectoverview.js
@@ -1,59 +1,58 @@
-import { checkStorage } from "../applogic/LocalStorage/checklocalstorage";
-import saveToStorage from "../applogic/LocalStorage/savetolocalstorage";
+import { checkStorage } from '../applogic/LocalStorage/checklocalstorage';
+import saveToStorage from '../applogic/LocalStorage/savetolocalstorage';
 
 function getActiveProject() {
-    const projects = document.getElementsByClassName('projectListItem');
-        for (let i = 0; i < projects.length; i++) {
-            if (projects[i].classList.contains('active')) {
-                return i
-            } else {
-                return 0
-            }
-        }
+  let result = 0;
+  const projects = document.getElementsByClassName('projectListItem');
+  for (let i = 0; i < projects.length; i += 1) {
+    if (projects[i].classList.contains('active')) {
+      result = i;
+    }
+  }
+  return result;
 }
 
-//Need to figure out how to correctly get the current active project!!!!
+// Need to figure out how to correctly get the current active project!!!!
 
 function populateDisplay() {
-    const activeProject = getActiveProject();
-    const displayArea = document.getElementById('body');
-    const userProjects = checkStorage();
+  const activeProject = getActiveProject();
+  const displayArea = document.getElementById('body');
+  const userProjects = checkStorage();
 
-    displayArea.innerHTML = "";
+  displayArea.innerHTML = '';
 
-    const projectTitle = document.createElement('p');
-    projectTitle.textContent = `${activeProject.name}`;
-    projectTitle.classList.add('projectTitle');
+  const projectTitle = document.createElement('p');
+  projectTitle.textContent = `${activeProject.name}`;
+  projectTitle.classList.add('projectTitle');
 
-    const projectTasksArea = document.createElement('div');
-    projectTasksArea.classList.add('projectTasksArea');
+  const projectTasksArea = document.createElement('div');
+  projectTasksArea.classList.add('projectTasksArea');
 
-    const tasks = userProjects[activeProject].toDoList;
-    
-    if (tasks.length > 0) {
-        for (let i = 0; i < tasks.length; i++) {
-            let button = document.createElement('button');
-            button.classList.add('task');
-            button.textContent = tasks[i];
-            projectTasksArea.appendChild(button);
-        }
-    }   else {
-        }
-    
-    const newTaskButton = document.createElement('button');
-    newTaskButton.classList.add('task');
-    newTaskButton.setAttribute('id', 'newTaskButton');
-    newTaskButton.addEventListener('click', () => {
-        const project = userProjects[activeProject];
-        project.addToDo('New Task');
-        saveToStorage(project);
-    })
-    projectTasksArea.appendChild(newTaskButton);
+  const tasks = userProjects[activeProject].toDoList;
 
-    displayArea.appendChild(projectTitle);
-    displayArea.appendChild(projectTasksArea);
+  if (tasks.length > 0) {
+    for (let i = 0; i < tasks.length; i += 1) {
+      const button = document.createElement('button');
+      button.classList.add('task');
+      button.textContent = tasks[i];
+      projectTasksArea.appendChild(button);
+    }
+  }
 
-    return displayArea;
+  const newTaskButton = document.createElement('button');
+  newTaskButton.classList.add('task');
+  newTaskButton.setAttribute('id', 'newTaskButton');
+  newTaskButton.addEventListener('click', () => {
+    const project = userProjects[activeProject];
+    project.addToDo('New Task');
+    saveToStorage(project);
+  });
+  projectTasksArea.appendChild(newTaskButton);
+
+  displayArea.appendChild(projectTitle);
+  displayArea.appendChild(projectTasksArea);
+
+  return displayArea;
 }
 
 export default populateDisplay;
